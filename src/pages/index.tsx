e1cc327ce20e636a2d9ce7c2290d52fe2d3727c2
Lets TN <leticia.teix.nas@gmail.com>
2022-01-20 16:46:59 -0300

    feat: cria o componente de mapa com o leaflet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
-import Main from 'components/Main'
+import dynamic from 'next/dynamic'
+
+// usa o dynamic imports para não incluir um módulo no lado do servidor (SSR)
+// fazendo isso nesse caso pq o mapa tem o window, que funciona só no browser
+// sem isso vai quebrar falando que o window n existe (mas existe, só no browser)
+const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return <Main />
+  return <Map />
 }
